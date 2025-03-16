b6c34caddd2e183f299f66829b0b373e3e93d197
TheFJcurve <sargunbhatti27@gmail.com>
2025-03-16 17:10:32 -0400

    restructing project
    - working with a frontend and a backend
    - backend will posses logic for backtesting and how algorithms should be tested
    - frontend will show the tested logic in a friendly way

--- setup.sh
+++ setup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# Python setup
+projectDir=$(git rev-parse --show-toplevel)
+frontendDir="$projectDir/frontend"
+backendDir="$projectDir/backend"
+currentDir=$(pwd)
+
+# Setting up the frontend
+cd "$frontendDir" 
+npm i
+
+# Setting up the backend
+cd "$backendDir" 
 python3.9 -m venv venv 
-source venv/bin/activate 
+source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+
+cd "$currentDir"
